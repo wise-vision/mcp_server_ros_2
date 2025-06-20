6c553ffc9c5ada55abb5a4cdd7b673ff82f68aed
czarekk215 <czarekmadziewczyne@gmail.com>
2025-06-20 09:23:14 +0200

    Enable image output in tools

--- server/toolhandler.py
+++ server/toolhandler.py
@@ -11,6 +11,7 @@ from collections.abc import Sequence
 from mcp.types import (
     Tool,
     TextContent,
+    ImageContent,
     EmbeddedResource,
 )
 
@@ -22,5 +23,5 @@ class ToolHandler:
     def get_tool_description(self) -> Tool:
         raise NotImplementedError()
 
-    def run_tool(self, args: dict) -> Sequence[TextContent | EmbeddedResource]:
+    def run_tool(self, args: dict) -> Sequence[TextContent | ImageContent | EmbeddedResource]:
         raise NotImplementedError()
